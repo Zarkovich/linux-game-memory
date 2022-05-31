1f7cdef0ee58312bbb8015f81d7c2d1136a6245d
Gustavo Vieira <gustavovieirazu@gmail.com>
2022-05-31 00:08:45 -0300

    Upload

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+const cards = document.querySelectorAll(".card");
+const resetButton = document.querySelector(".reset");
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+//função para virar carta
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
+}
+
+//função que checa se as cartas são iguais
+function checkForMatch() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
+
+  unflipCards();
+}
+
+//função que desabilita as cartas
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+//funcão que desvira as cartas
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1500);
+}
+
+//função que reseta o tabuleiro
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+//função que embaralha as cartas
+(function shuffle() {
+  cards.forEach((card) => {
+    let ramdomPosition = Math.floor(Math.random() * 12);
+    card.style.order = ramdomPosition;
+    card.addEventListener("click", flipCard);
+  });
+})();
+
+function resetGame() {
+  cards.forEach((card) => {
+    card.addEventListener("click", flipCard);
+    card.classList.remove("flip");
+    resetBoard();
+    setTimeout(function () {
+      let ramdomPosition = Math.floor(Math.random() * 12);
+      card.style.order = ramdomPosition;
+    }, 1000);
+    console.log("clickei");
+  });
+}
+
+resetButton.addEventListener("click", resetGame);
